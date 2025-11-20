88590f9f7bc469714f52a300b95e5514866625c5
Igor <anyushev@gmail.com>
2025-11-20 23:47:59 +0300

    First version

---
+++ generate_new_fake_data.py
@@ -0,0 +1,16 @@
+import sqlite3
+from faker import Faker
+
+path_my_bd = r'C:\Users\user\AppData\Roaming\DBeaverData\workspace6\.metadata\sample-database-sqlite-1\Chinook.db'
+
+
+def connect_db(path_bd):
+    pass
+
+
+def generate_some_fake_data(num, cursor):
+    pass
+
+
+def commit_and_close(connection):
+    pass
